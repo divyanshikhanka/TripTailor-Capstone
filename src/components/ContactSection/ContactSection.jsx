c2f5e680da19fe905e4d1e96b979512938a48df1
Divyanshi Khanka <divyanshikhanka@Divyanshis-MacBook-Pro.local>
2025-05-06 23:13:44 +0530

    fixed some spellings

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -30,7 +30,7 @@ const ContactSection = () => {
       isSubmitting: true
     });
     
-    // Simulate form submission
+
     setTimeout(() => {
       setFormStatus({
         message: 'Thank you for your message! We\'ll get back to you soon.',
